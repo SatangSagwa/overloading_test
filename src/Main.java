371b16c341b3f1f30176564e6787288a3a9197ba
SatangSagwa <emilia.ivarsson@live.se>
2024-09-16 09:46:36 +0200

    Started method overloading ex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int score = calcScore("Em", 5);
+        System.out.println("Score: " + score);
+        score = calcScore(3);
+        System.out.println("Score: " + score);
+    }
+
+    public static int calcScore(String name, int score) {
+        System.out.println("Player " + name + "'s score is " + score);
+        return score * 1000;
+    }
+
+    public static int calcScore(int score) {
+        System.out.println("Unnamed player's score is " + score);
+        return score * 1000;
     }
 }
